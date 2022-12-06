37b2cccbc573c3e81cbdb2eaec4cb1e59f9befe5
박민성 <102217591+minsung060607@users.noreply.github.com>
2022-12-06 17:54:17 +0900

    Create test.c

---
+++ test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int GDP;
+char country[20][20];
+int num(int n){
+    num(n-1);
+    if(i<0) return 0;
+    else{
+        for(int i=0;i<20;i++){
+        scanf("%c",&country[n][i]);
+    }
+    }
+}
+int main(){
+
+
+
+    return 0;
+}
